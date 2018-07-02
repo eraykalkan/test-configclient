f33844858633656c87bd72a08d354d0e050b32b3
eraykalkan <eraykalkan@gmail.com>
2018-07-03 00:39:54 +0300

    Initial commit

---
+++ src/main/java/com/configclient/configclient/RateController.java
@@ -0,0 +1,36 @@
+package com.configclient.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RefreshScope
+public class RateController {
+
+    @Value("${rate}")
+    String rate;
+
+    @Value("${lanecount}")
+    String laneCount;
+
+    @Value("${tollstart}")
+    String tollStart;
+
+    @Value("${connstring}")
+    String connString;
+
+    @RequestMapping("/rate")
+    public String getRate(Model m){
+        m.addAttribute("rateamount",rate);
+        m.addAttribute("lanes",laneCount);
+        m.addAttribute("tollstart",tollStart);
+        m.addAttribute("connstring",connString);
+
+        //name of the view
+        return "rateview";
+
+    }
+}
